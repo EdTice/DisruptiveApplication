508247a60e811e8e8a765287088b076e4eba8e50
EdTice <etice@coverity.com>
2022-07-08 12:49:04 -0400

    Create Disruptive.java

---
+++ src/com/example/Disruptive.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Disruptive
+{
+  public void beDisruptive(String str)
+  {
+    System.out.println("Be Disruptive");
+    System.out.println("Be-E Disruptive");
+    System.out.println("B-E D-I-S-R-U-P-T-I-V-E");
+  }
+}
