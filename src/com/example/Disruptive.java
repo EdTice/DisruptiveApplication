24f677de54eae8662e5bb1ca20951ece896bbda8
EdTice <etice@coverity.com>
2022-07-12 08:31:30 -0400

    Update Disruptive.java

--- src/com/example/Disruptive.java
+++ src/com/example/Disruptive.java
@@ -1,14 +1,17 @@
 package com.example;
 
-// Last Modified 2022-07-11 17:43
-
 public class Disruptive
 {
-  // TODO: Be more disruptive
+  java.net.PasswordAuthentication pa;
+
   public void beDisruptive(String str)
   {
     System.out.println("Be Disruptive");
     System.out.println("Be-E Disruptive");
     System.out.println("B-E D-I-S-R-U-P-T-I-V-E");
+    str=str;
+    
+    String pw = new String(pa.getPassword());
+    System.out.println("Your access token is: "+pw);
   }
 }
